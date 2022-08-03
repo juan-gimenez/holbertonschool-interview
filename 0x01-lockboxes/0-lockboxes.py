068e2161d619d9d2d323052bf70a4d6191184a3d
juan-gimenez <juancitogimenez0@gmail.com>
2022-08-03 18:11:48 +0000

    task lockboxes 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+lockboxes interview prep
+"""
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened. """
+    lenb = len(boxes)
+    list = [0]
+    for i in list:
+        for j in boxes[i]:
+            if j not in list:
+                if j < lenb:
+                    list.append(j)
+    if len(list) == lenb:
+        return True
+    return False
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+lockbock exercise
