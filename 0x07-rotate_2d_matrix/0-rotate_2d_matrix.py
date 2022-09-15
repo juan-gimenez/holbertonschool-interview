5c5f952e5f3cdb6ee3c8656974e69ae4742f96f4
juan-gimenez <juancitogimenez0@gmail.com>
2022-09-15 16:20:53 +0000

    task 0 to check

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def rotate_2d_matrix(matrix):
+    """ rotates a 2d matrix"""
+
+    lenmatrix = len(matrix)
+    for i in range(lenmatrix):
+        for j in range(i, lenmatrix):
+            matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+
+    x=len(matrix)
+    for i in range(x//2):
+        for j in range(x):
+            matrix[j][i],matrix[j][x-1-i]=matrix[j][x-1-i],matrix[j][i]
